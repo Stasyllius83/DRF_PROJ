0db83256e1ceb4abe8d2281b7ff37e1c961b5239
Stas <stasyllius@gmail.com>
2024-02-20 23:29:25 +0300

    change serializer

--- materials/serializers.py
+++ materials/serializers.py
@@ -13,7 +13,7 @@ class LessonSerializer(serializers.ModelSerializer):
 
 class CourseSerializer(serializers.ModelSerializer):
     lesson_count = serializers.SerializerMethodField()
-    lessons = LessonSerializer(source='lesson_set', many=True)
+    lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
 
     def get_lesson_count(self, instance):
         if instance.lesson_set.all().first():
